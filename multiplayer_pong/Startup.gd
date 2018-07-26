4dc9082a21dc346da50bac0d458da237b310a148
GammaGames <jesse007@ymail.com>
2018-07-25 23:55:20 -0600

    update export

--- multiplayer_pong/Startup.gd
+++ multiplayer_pong/Startup.gd
@@ -1,10 +1,8 @@
 extends Node
 
 func _ready():
-	var args = OS.get_cmdline_args()
-	if args.size() > 0 && args[0] == "-s":
+	var args = Array(OS.get_cmdline_args())
+	if args.has("-s"):
 		get_tree().change_scene("res://server.tscn")
 	else:
 		get_tree().change_scene("res://pong.tscn")
-
-	pass
